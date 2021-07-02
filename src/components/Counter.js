059a13ac4f004ec59a7e5d857037c7c0360a72ef
apeksha2697 <apekshasingh664@gmail.com>
2021-07-02 07:55:42 +0530

    added toggle handler

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,10 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
+  const show = useSelector(state => state.showCounter);
   const counter = useSelector(state => state.counter);
   
   const increamentHandler = () => {
@@ -20,7 +23,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={increamentHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment By 5</button>
